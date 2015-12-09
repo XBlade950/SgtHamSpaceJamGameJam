4ec35c66b7c501a6b06169ec34660f43185531b1
XBlade950 <alexander.ledet95@yahoo.com>
2015-12-08 18:33:32 -0600

    working on shield stuff

--- Assets/Assets_alexander/Scripts/Shield.cs
+++ Assets/Assets_alexander/Scripts/Shield.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class Shield : MonoBehaviour {
 
+    [SerializeField]
+    private float lifeTime;
+    private float startTime;
 	// Use this for initialization
 	void Start () {
-	
+        startTime = Time.deltaTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 
    
