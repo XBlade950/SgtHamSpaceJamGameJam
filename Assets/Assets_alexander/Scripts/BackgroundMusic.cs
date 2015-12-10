6e2c43342213c955fa9be8aa357157ac9b0d3e19
XBlade950 <alexander.ledet95@yahoo.com>
2015-12-09 21:30:39 -0600

    added some jams to the jam

---
+++ Assets/Assets_alexander/Scripts/BackgroundMusic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    AudioSource bgm;
+    private bool playing = true;
+	// Use this for initialization
+	void Start () {
+        bgm = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject obj = col.gameObject;
+
+        if (obj.tag == "Player" && playing)
+        {
+            bgm.Stop();
+            playing = false;
+        }
+    }
+}
